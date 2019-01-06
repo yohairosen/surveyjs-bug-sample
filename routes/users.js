38a929afe7dd037e5bf8cb0363e15baf40ae12f1
Yohai Rosen <yohai.roz@gmail.com>
2019-01-06 22:17:45 +0200

    testing

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-
+// http://surveyjs.io/api/MySurveys/getActive?accessKey=b4a00480f27c438596d828bc42da477a
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
   request({
     uri: 'http://surveyjs.io/api/MySurveys/getActive',
     qs: {
-      accessKey: 'a791ca62357f470d913d88d353e351e0',
+      accessKey: 'b4a00480f27c438596d828bc42da477a',
       ownerId: 'keyops'
     }
   }, function (error, response, body) {
     console.log(error, response);
-  }).pipe(res);
+
+    if(error)
+     return  res.send(error);
+
+    
+    res.send(response);
+  })
 
 });
 
